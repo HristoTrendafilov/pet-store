28e3e31a8a83b92fbd3c789cea513ba70cee2c4c
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2023-12-25 17:28:27 +0200

    Question and check for folder structure changes

--- pet-store-react-hooks/src/infrastructure/api-types.ts
+++ pet-store-react-hooks/src/infrastructure/api-types.ts
@@ -1,6 +1,7 @@
 export interface PetListItem {
   petId: number;
   petName: string;
+  // Question: Sence the addedDate comes from the api as string, should i change the type to string and pass it as Date where i have to?
   addedDate: Date;
   kind: number;
 }
