c605bc9fc22e481e57e4e3a4003cf67f198366f7
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2024-02-13 19:21:41 +0200

    Home component tests

--- pet-store-react-hooks/src/Home/Home.test.jsx
+++ pet-store-react-hooks/src/Home/Home.test.jsx
@@ -1,14 +1,14 @@
-import { setupServer } from 'msw/node';
-import { handlers } from '~/Tests/handlers';
+test('Error message is shown on failed network request', () => {});
+test('Pet table rows are rendered properly', () => {
+  // 1. The length of the rows should be exatctly the count fetched from the mock
+  // 2. Added date column should be in the corrent format (dd MMM yyyy)
+  // 3. Kind column should have the string representations of the pet kind
+  // 4. Every row should have 2 buttons with the text - 'View / Edit' and 'Delete'
+});
+// Think about the case in <Home/> component
+test('Add pet button is disabled if fetching pet kinds has failed', () => {});
 
-const server = setupServer(...handlers);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
-
-test('Loading indicator is shown while fetching all pets and then hidden', () => {});
-
-test('Add pet button is disabled while fetching all pets and then enabled', () => {});
-
-test('The pet table should have exact rows as the fetched pets', () => {});
+// Questions
+// 1. Should i validate the text of the buttons and pet table header?
+// 2. Should i validate the structure of the table (exact amount of columns and the text in them)?
+// 3.
