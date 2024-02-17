860bd09922fbacc8343657a53cec7b2ad52f2071
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2024-02-17 14:55:46 +0200

    Home component tests

---
+++ pet-store-react-hooks/src/Home/Home.test.tsx
@@ -0,0 +1,13 @@
+test('Table header is displayed', () => {});
+test('Add pet button is displayed', () => {});
+test('Add pet button is disabled while fetching pets and then enabled', () => {});
+
+test('The table is displayed', () => {});
+test('The table columns count and text inside of them is correct', () => {});
+test('The table rows cells are visualized correctly and there are two buttons for every row', () => {});
+
+test('New pet modal is shown on Add pet button click and then closed', () => {});
+test('View/Edit pet modal is shown on row button click and then closed', () => {});
+test('Delete pet modal is shown on row button click and then closed', () => {});
+
+test('Error message is displayed and Add pet button is disabled on failed network request', () => {});
