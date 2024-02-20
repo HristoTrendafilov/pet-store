196d634ed3aea9bfc57cf1b8d2c19330ecc7c70c
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2024-02-20 18:03:41 +0200

    Home tests

--- pet-store-react-hooks/src/Home/Home.test.tsx
+++ pet-store-react-hooks/src/Home/Home.test.tsx
@@ -2,9 +2,8 @@ test('Table header is displayed', () => {});
 test('Add pet button is displayed', () => {});
 test('Add pet button is disabled while fetching pets and then enabled', () => {});
 
-test('The table is displayed', () => {});
-test('The table columns count and text inside of them is correct', () => {});
-test('The table rows cells are visualized correctly and there are two buttons for every row', () => {});
+test('The table is displayed and columns count and text inside of them is correct', () => {});
+test('All table rows are rendered, cell values are visualized correctly and each row has two buttons', () => {});
 
 test('New pet modal is shown on Add pet button click and then closed', () => {});
 test('View/Edit pet modal is shown on row button click and then closed', () => {});
