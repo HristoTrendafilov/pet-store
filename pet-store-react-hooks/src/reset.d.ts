4a7b3b29951294039624d9e1e1a72c39b4fc6af1
Hristo Kolev <hristo.a.kolev@gmail.com>
2024-03-30 18:03:18 +0200

    updates from parent project

--- pet-store-react-hooks/src/reset.d.ts
+++ pet-store-react-hooks/src/reset.d.ts
@@ -2,3 +2,4 @@
 /// <reference types="@total-typescript/ts-reset/filter-boolean" />
 /// <reference types="@total-typescript/ts-reset/is-array" />
 /// <reference types="@total-typescript/ts-reset/json-parse" />
+/// <reference types="@total-typescript/ts-reset/storage" />
